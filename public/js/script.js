741a066f5206e909e33810e52b17dc1557208bc9
fkarim20 <fouzan.mdkarim@gmail.com>
2021-08-15 01:32:15 -0400

    next commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,21 @@
 function calcPrice() {
   let price = 0.0;
-  price =
-    parseFloat(document.getElementById().value) *
-    parseFloat(document.getElementById().value);
+  let dOne = document.querySelector(".checkIn").value;
+  let dTwo = document.querySelector(".checkOut").value;
 
-  return price;
+  let dateOne = new Date(dOne);
+  let dateTwo = new Date(dTwo);
+  const oneDay = 1000 * 60 * 60 * 24;
+  let timeInMs = dateTwo.getTime() - dateOne.getTime();
+  const diffInDays = Math.round(timeInMs / oneDay);
+
+  let base = document.querySelector(".subPrice").innerHTML;
+  let basePrice = parseFloat(base.substr(1, base.length));
+  let subtotal = document.querySelector(".subPrice");
+  let taxes = document.querySelector(".taxes");
+  let total = document.querySelector(".totalPrice");
+
+  subtotal.textContent = basePrice;
+  taxes.textContent = basePrice;
+  total.textContent = diffInDays;
 }
